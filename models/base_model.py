9c6f9ecc1453455ab66db75dc415458232408c14
KGIveso <mogoboyagiven@gmail.com>
2023-07-13 02:57:30 -0700

    base_line

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-"""
-Module contains a class BaseModel that defines all common
-attributes/methods for other classes
-"""
 import uuid
 import datetime
 
-class BaseModel(uuid, datetime):
+class BaseModel:
     """ Defines all common attributes/methods for other classes """
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = str('')
-        self.updated_at = str('')
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
     
     def __str__(self):
         """
-        Prints the current instance of the class
+        Returns the string representation of the current instance
         """
-        str_object = '{[]} {()} {}'.format(self.__class__.__name__, self.id, self.__dict__)
-        print(str_object)
-
-    def __dict__(self):
-        """
-        Dictionary representation of the current instance
-        """
-        pass
+        return '{} {} {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
         Updates public instance attribute 'updated_at' with current datetime
         """
-        pass
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """
         Returns a dictionary containing all key/values of '__dict__' of the
         instance.
         """
-        pass
+        return self.__dict__
