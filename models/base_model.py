1a3ea71729e4333c7f04d2ebab1b7ec0dc66419a
reckafella <ethanmuthoniw6@gmail.com>
2023-07-14 18:05:00 +0300

    update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -24,24 +24,24 @@ class BaseModel:
                 if key != '__Class__':
                     setattr(self, key, value)
 
-                if kwargs.get('created_at', None) is not None and
-                type(kwargs['created_at']) is str:
-                    self.created_at = datetime.strptime(
-                        kwargs['created_at'], time_format)
+                if kwargs.get('created_at', None) is not None and\
+                    type(kwargs['created_at']) is str:
+                        self.created_at = datetime.strptime(
+                            kwargs['created_at'], time_format)
                 else:
                     self.created_at = datetime.datetime.now()
 
-                if kwargs.get('updated_at', None) is not None and
-                type(kwargs['updated_at']) is str:
-                    self.created_at = datetime.strptime(
-                        kwargs['updated_at'], time_format)
+                if kwargs.get('updated_at', None) is not None and\
+                    type(kwargs['updated_at']) is str:
+                        self.created_at = datetime.strptime(
+                            kwargs['updated_at'], time_format)
                 else:
                     self.updated_at = datetime.datetime.now()
 
-                if (kwargs.get('id', None) is None:
+                if (kwargs.get('id', None)) is None:\
                     self.id=str(uuid.uuid4())
 
-     def __str__(self):
+    def __str__(self):
         """
         Returns the string representation of the current instance
         """
@@ -52,7 +52,7 @@ class BaseModel:
         """
         Updates public instance attribute 'updated_at' with current datetime
         """
-        self.updated_a=datetime.datetime.now()
+        self.updated_a = datetime.datetime.now()
 
     def to_dict(self):
         """
@@ -64,11 +64,11 @@ class BaseModel:
         'created_at' and 'updated_at' are converted to string objects in ISO
         format.
         """
-        obj_dict=self.__dict__.copy()
-        obj_dict['__class__']=self.__class__.__name__
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
 
         for key, value in obj_dict.items():
             if isinstance(value, datetime.datetime):
-                obj_dict[key =value.isoformat()
+                obj_dict[key] = value.isoformat()
 
         return {k: v for k, v in obj_dict.items() if v is not None}
