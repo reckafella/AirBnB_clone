ec76d2bdebfce522e4d5694b39531084d6938bf6
KGIveso <mogoboyagiven@gmail.com>
2023-07-16 04:34:51 -0700

    other models files

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+""" Amenity to inherit from BaseModel"""
+
+class Amenity(BaseModel):
+    """The amenity class"""
+    name = ''
+
---
+++ models/state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""State to inherit from basemodel"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """The state class"""
+    name = ''
