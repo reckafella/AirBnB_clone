bcb621a6215ae7ef6253912275c2924786381fc7
KGIveso <mogoboyagiven@gmail.com>
2023-07-16 18:51:51 -0700

    revised test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,34 +1,97 @@
 #!/usr/bin/python3
-
-"""
-Module contains test cases for the BaseModel
-"""
-import unittest
+""" """
 from models.base_model import BaseModel
-import models.base_model as bs_model
+import unittest
+import datetime
+from uuid import UUID
+import json
+import os
 
+class test_basemodel(unittest.TestCase):
+    """ """
+    
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
 
-class TestBaseModel(unittest.TestCase):
-    """
-    class implements test cases for the base model
-    """
-    def test_base_model_docs(self):
-        """
-        Tests for Base Model docs
-        """
-        docs = bs_model.__doc__
-        self.assertTrue(len(docs) > 1)
-        self.assertTrue(len(BaseModel.__doc__) > 1)
+    def setUp(self):
+        """ """
+        pass
 
-    def test_base_model_attrs(self):
-        """
-        Tests for Base Model attributes
-        """
-        bm1 = BaseModel()
-        bm2 = BaseModel()
-        self.assertTrue(hasattr(bm1, 'id'))
-        self.assertTrue(hasattr(bm1, 'created_at'))
-        self.assertTrue(hasattr(bm1, 'updated_at'))
-        self.assertNotEqual(bm1.id, bm2.id)
-        self.assertIsInstance(bm1.id, str)
-        self.assertIsInstance(bm2.id, str)
+    def tearDown(self):
+        try:
+            os.remove('file.json')
+        except:
+            pass
+
+    def test_default(self):
+        """ """
+        i = self.value()
+        self.assertEqual(type(i), self.value)
+
+    def test_kwargs(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        new = BaseModel(**copy)
+        self.assertFalse(new is i)
+
+    def test_kwargs_int(self):
+        """ """
+        i = self.value()
+        copy = i.to_dict()
+        copy.update({1: 2})
+        with self.assertRaises(TypeError):
+            new = BaseModel(**copy)
+
+    def test_save(self):
+        """ Testing save """
+        i = self.value()
+        i.save()
+        key = self.name + "." + i.id
+        with open('file.json', 'r') as f:
+            j = json.load(f)
+            self.assertEqual(j[key], i.to_dict())
+
+    def test_str(self):
+        """ """
+        i = self.value()
+        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id, i.__dict__))
+
+    def test_todict(self):
+        """ """
+        i = self.value()
+        n = i.to_dict()
+        self.assertEqual(i.to_dict(), n)
+
+    def test_kwargs_none(self):
+        """ """
+        n = {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**n)
+ 
+    def test_kwargs_one(self):
+        """ """
+        n = {'Name': 'test'}
+        with self.assertRaises(KeyError):
+            new = self.value(**n)
+
+    def test_id(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.id), str)
+
+    def test_created_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.created_at), datetime.datetime)
+    
+    def test_updated_at(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.updated_at), datetime.datetime)
+        n = new.to_dict()
+        new = BaseModel(**n)
+        self.assertFalse(new.created_at == new.updated_at)
